3def32f2f2e1f58fe14744b04bd0aa58e1079ae1
HugoPaz417 <hugopaz417@gmail.com>
2022-12-17 20:27:11 -0500

    Tarea convertidor

--- src/bimestre2/semana1/semana2/Convertidor.java
+++ src/bimestre2/semana1/semana2/Convertidor.java
@@ -6,19 +6,23 @@ public class Convertidor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n_binario []= new int[6];
-        int suma = 0;
+        double suma = 0;
+        double n_multiplacion = 2;
+        double acumulador= 0;
         System.out.println("Ingrese un número binario de hasta 6 cifras");
         for (int indice = 0; indice < n_binario.length; indice++) {
+            System.out.println("Ingrese la cifra "+(indice+1));
             int n = sc.nextInt();
             if (n<0 || n>1) {
                 System.out.println("Error, no es un numero binario");
                 break;
             }
             n_binario [indice] = n;
-            for (int exponente = 0; exponente < indice; exponente++) {
-                suma = suma+(n*(2*exponente));
+            for (int x = n_binario.length, exponente = 0; x > indice; x--, exponente++) {
+                suma =(n_binario[indice])*(Math.pow(n_multiplacion,exponente));
             }
-            System.out.println(suma);
+            acumulador = acumulador+suma;
         }
+        System.out.println(acumulador);
     }
 }
